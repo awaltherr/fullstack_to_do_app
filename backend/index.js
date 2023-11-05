6ea9b2b2596ad4587c6fe93047db4e15edea85c3
awaltherr <anton.walther@live.com>
2023-11-05 13:43:18 +0100

    Implemented logic so the frontend works with database

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,57 @@ const client = new Client({
 
 client.connect();
 
+app.use(express.json());
+
 app.get("/api", async (_request, response) => {
   const { rows } = await client.query("SELECT * FROM to_do");
   response.send(rows);
+});
+
+app.post("/api", async (request, response) => {
+  const { task } = request.body;
+  if (task) {
+    const query = "INSERT INTO to_do (task) VALUES ($1) RETURNING *";
+    const values = [task];
+
+    try {
+      const { rows } = await client.query(query, values);
+      response.status(201).json(rows[0]);
+    } catch (error) {
+      console.error("Error inserting data:", error);
+      response
+        .status(500)
+        .json({ error: "Something went wrong inserting data" });
+    }
+  } else {
+    response.status(400).json({ error: "Invalid data" });
+  }
+});
+
+app.delete("/api/:taskId", async (request, response) => {
+  const taskId = request.params.taskId;
+
+  if (!taskId) {
+    response.status(400).json({ error: "Invalid ID on task" });
+    return;
+  }
+
+  const query = "DELETE FROM to_do WHERE id =$1";
+  const values = [taskId];
+
+  try {
+    const result = await client.query(query, values);
+    if (result.rowCount > 0) {
+      response.status(200).json({ message: "Task is deleted" });
+    } else {
+      response.status(404).json({ error: "Task was not found" });
+    }
+  } catch (error) {
+    connsole.error("Error accured removing task:", error);
+    response
+      .status(500)
+      .json({ error: "Something went wrong by removing task" });
+  }
 });
 
 app.use(express.static(path.join(path.resolve(), "public")));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,76 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleNewTask = () => {
+    fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ task: newTask }),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        fetch("/api")
+          .then((response) => response.json())
+          .then((newTask) => {
+            setData([...data, newTask]);
+            setNewTask("");
+          });
+      });
+  };
+
+  const handleDeleteTask = (taskId) => {
+    fetch(`/api/${taskId}`, {
+      method: "DELETE",
+    }).then(() => {
+      const updatedList = data.filter((item) => item.id !== taskId);
+      setData(updatedList);
+    });
+  };
 
   useEffect(() => {
     fetch("/api")
       .then((response) => response.json())
       .then((result) => {
-        console.log(result);
+        setData(result);
       });
   }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Räknare: {count}
+        <h3>Todo App</h3>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Add a new task..."
+        />
+        <button onClick={handleNewTask} disabled={newTask.trim() === ""}>
+          Add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              {item.task}{" "}
+              {!item.completed && (
+                <>
+                  <button onClick={() => handleDeleteTask(item.id)}>
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
